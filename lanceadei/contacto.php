9cd6443ecb7347cca2776afb38c0dc2a1de5715b
oscar <oacorral@gmail.com>
2020-08-05 18:32:31 +0200

    Bump lodash from 4.17.15 to 4.17.19 in /twentytwenty
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.15 to 
    4.17.19.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.15...4.17.19)
    
    Signed-off-by: dependabot[bot] <support@github.com>

---
+++ lanceadei/contacto.php
@@ -0,0 +1,13 @@
+<!-- Archivo de cabecera global de Wordpress -->
+<?php get_header(); ?>
+<!-- Contenido de página de inicio -->
+<?php if(have_posts()): the_post(); ?>
+  <section>
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+  </section>
+<?php endif; ?>
+<!-- Archivo de barra lateral por defecto -->
+<?php get_sidebar(); ?>
+<!-- Archivo de pie global de Wordpress -->
+<?php get_footer(); ?>
