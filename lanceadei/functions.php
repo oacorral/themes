de7ea887e29bd45749903069fd87367db22d9e15
oscar <oacorral@gmail.com>
2020-08-05 18:07:39 +0200

    Avanzando

--- lanceadei/functions.php
+++ lanceadei/functions.php
@@ -4,7 +4,11 @@
 */
 function mis_menus()
 {
-    register_nav_menus(array('navegation'=>__('Menú de navegación'),));
+    register_nav_menus(
+        array(
+        'navegation'=>__('Menú de navegación'),
+      )
+    );
 }
 add_action('init', 'mis_menus');
 
@@ -12,3 +16,31 @@ add_action('init', 'mis_menus');
 * Crear una zona de Widgets que podremos gestionar desde el administrador de
 * Wordpress  fdsdfas
 */
+function mis_widgets()
+{
+    register_sidebar(
+        array(
+    'name'         =>__('Sidebar'),
+    'id'           => 'sidebar',
+    'before_widget'=>'<div class="widget">',
+    'after_widget' =>'</div>',
+    'before_title' =>'<h3>',
+    'after_title'  =>'</h3>',
+  )
+    );
+}
+add_action('init', 'mis_widgets');
+
+/**
+ * Filtrar resultados de búsqueda para que solo muestre
+ * posts en el listado
+ */
+
+function buscar_solo_posts($query)
+{
+    if ($query->is_search) {
+        $query->set('post_type', 'post');
+    }
+    return $query;
+}
+add_filter('pre_get_posts', 'buscar_solo_posts');
