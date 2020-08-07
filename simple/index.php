0ece9457e079cd6fe65d8fe849042b92c5917793
Oscar <41696902+oacorral@users.noreply.github.com>
2020-08-07 12:49:15 +0200

    Header común

--- simple/index.php
+++ simple/index.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    <?php wp_head(); ?>
-  </head>
-  <body>
 
-  </body>
-</html>
+<?php get_header(); ?>
+    <div class="main">
+      <div class="container">
+        <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+          <h3><?php the_title(); ?></h3>
+          <div class="meta">
+            Escrito por <?php the_author(); ?> el <?php the_time('l j \d\e F \d\e Y \a \l\a\s H:i:s'); ?>
+          </div>
+          <?php the_content(); ?>
+        <?php endwhile; ?>
+        <?php else :?>
+          <?php echo wpautop('Es triste decir que no hay ni un simple post escrito. ¡Qué vergüenza!'); ?>
+        <?php endif; ?>
+      </div>
+    </div>
+<?php get_footer(); ?>
