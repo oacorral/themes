99cce2363a250f9f43cba280fd6ebc87f260f982
oscar <oacorral@gmail.com>
2020-08-11 11:57:08 +0200

    Se añade el enlace a la hoja de estilos en index.php. Se crean los div para el contenerdor general, el principal y el de la barra delateral. Se dan algunos estilos iniciales.

--- oacsimple/index.php
+++ oacsimple/index.php
@@ -3,24 +3,37 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <?php wp_head(); ?>
   </head>
   <body>
-    <header>
-      <h1><?php bloginfo('name'); ?></h1>
-      <span><?php bloginfo('description'); ?></span>
-    </header>
-    <?php if (have_posts()) : ?>
-      <?php while (have_posts()) : the_post() ?>
-        <div class="post">
-          <h3><?php the_title(); ?></h3>
-          <?php the_excerpt(); ?>
-        </div>
-      <?php endwhile; ?>
-    <?php else : ?>
-        <p>¡Ups! No tenemos ni una simple entrada. Estamos trabajando en ello.</p>
-    <?php endif; ?>
+    <div class="container">
+      <div class="main">
+        <header>
+          <h1><?php bloginfo('name'); ?></h1>
+          <span><?php bloginfo('description'); ?></span>
+        </header>
+        <nav>
+
+        </nav>
+        <?php if (have_posts()) : ?>
+          <?php while (have_posts()) : the_post() ?>
+            <div class="post">
+              <h3><?php the_title(); ?></h3>
+              <?php the_excerpt(); ?>
+            </div>
+          <?php endwhile; ?>
+        <?php else : ?>
+            <p>¡Ups! No tenemos ni una simple entrada. Estamos trabajando en ello.</p>
+        <?php endif; ?>
+      </div>
+      <div class="sidebar">
+
+      </div>
+    </div>
     <footer>
-      <small>&copy;<?php bloginfo('name'); ?> <?php the_time('Y'); ?></small>
+      <small>&copy;<?php the_time('Y'); ?> <?php bloginfo('name'); ?> </small>
     </footer>
+    <?php wp_footer(); ?>
   </body>
 </html>
