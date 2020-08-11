c4c2168dbe0a74809e187f7033df1d30ca4db810
Oscar <41696902+oacorral@users.noreply.github.com>
2020-08-11 18:30:19 +0200

    Se añade el dato sobre el autor y la fecha de escritura, así como enlaces al post. Se crea el botón leer más y se dan lo estilos oportunos.

--- oacsimple/index.php
+++ oacsimple/index.php
@@ -14,21 +14,29 @@
           <span><?php bloginfo('description'); ?></span>
         </header>
         <nav>
-
+          MENÚ DE NAVEGACIÓN AQUÍ
         </nav>
         <?php if (have_posts()) : ?>
           <?php while (have_posts()) : the_post() ?>
-            <div class="post">
-              <h3><?php the_title(); ?></h3>
+            <article class="post">
+              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+              Escrito por <a href="<?php the_author_url(); ?>"><?php the_author(); ?></a>  el <?php the_time('l j \d\e F \d\e Y'); ?>
+              <?php if (has_post_thumbnail()) : ?>
+                <div class="post-thumbnail">
+                  <?php the_post_thumbnail(); ?>
+                </div>
+              <?php endif; ?>
               <?php the_excerpt(); ?>
-            </div>
+              <br>
+              <a class="button" href="<?php the_permalink(); ?>">Leer más</a>
+            </article>
           <?php endwhile; ?>
         <?php else : ?>
             <p>¡Ups! No tenemos ni una simple entrada. Estamos trabajando en ello.</p>
         <?php endif; ?>
       </div>
       <div class="sidebar">
-
+        BARRA LATERAL AQUÍ
       </div>
     </div>
     <footer>
