3fec404f730cd26cc02a3aa639093a134403f092
Oscar <41696902+oacorral@users.noreply.github.com>
2020-08-31 16:04:53 +0200

    Vamos cambiando los textos a fuego por funciones de WordPress.

--- advanced-wp/index.php
+++ advanced-wp/index.php
@@ -2,16 +2,19 @@
 <html <?php language_attributes(); ?>">
 
 <head>
-  <meta charset="utf-8">
-  <title>Advanced WP Theme</title>
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title><?php bloginfo('name'); ?></title>
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+  <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?> >
+  <?php  get_stylesheet_uri(); ?>
   <header>
     <div class="container">
       <h1>
-        <a href="index.html">Advanced Wordpress Theme</a>
+        <a href="index.html"><?php bloginfo('name'); ?></a>
         <small>Another Wordpress Theme</small>
       </h1>
       <div class="h_right">
