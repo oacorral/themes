5b3edafb8eb6daae3b551e921ddfd66e1c83ff36
Oscar <41696902+oacorral@users.noreply.github.com>
2020-09-07 19:49:04 +0200

    Otro pasito

--- advanced-wp/index.php
+++ advanced-wp/index.php
@@ -34,45 +34,19 @@
   </nav>
   <div class="container content">
     <div class="main block">
-      <article class="post">
-        <h2>Blog Post 1</h2>
-        <p class="meta">Posted at 11:00 on May 9 by admin</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non tincidunt ante, eget pulvinar mauris. Integer commodo enim ac tincidunt molestie. In finibus, velit ac ultricies imperdiet, nunc lorem rhoncus eros, nec imperdiet dui
-          ipsum sit amet lectus. Nulla pulvinar, lectus in aliquet porttitor, lectus magna fermentum justo, viverra pretium sapien neque ut mauris. Donec sed ullamcorper ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-          inceptos himenaeos. Nulla congue pellentesque arcu, sit amet rutrum lorem bibendum eget. Nam quis quam gravida, placerat eros eu, ornare diam. Vestibulum consectetur orci sed lacus commodo pretium. Donec eu viverra lorem. Etiam facilisis
-          dapibus velit, non scelerisque eros aliquet ac. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-        <p>Morbi eros arcu, aliquet ac vehicula non, eleifend nec turpis. Cras in vulputate neque. Aliquam quis maximus tellus, vitae faucibus elit. Quisque consequat pellentesque lectus tempus pretium. Curabitur at metus et enim sollicitudin
-          convallis. Vestibulum tincidunt porta volutpat. Sed vel magna dui. Vestibulum elit quam, pharetra at mi sed, tincidunt facilisis nunc. Curabitur blandit nibh et risus pellentesque, nec placerat purus convallis. Quisque velit magna, mattis
-          eu magna ac, luctus tempus lorem. Nulla iaculis dolor malesuada elit consequat, ac sagittis ipsum mollis. Pellentesque a lacinia felis, sit amet facilisis felis. Cras pharetra nisl ac feugiat convallis. Suspendisse nec consectetur lectus.
-          Quisque metus est, venenatis in consectetur vel, auctor a ipsum.</p>
-        <a class="button" href="#">Read More</a>
-      </article>
-      <article class="post">
-        <h2>Blog Post 2</h2>
-        <p class="meta">Posted at 11:00 on May 9 by admin</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non tincidunt ante, eget pulvinar mauris. Integer commodo enim ac tincidunt molestie. In finibus, velit ac ultricies imperdiet, nunc lorem rhoncus eros, nec imperdiet dui
-          ipsum sit amet lectus. Nulla pulvinar, lectus in aliquet porttitor, lectus magna fermentum justo, viverra pretium sapien neque ut mauris. Donec sed ullamcorper ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-          inceptos himenaeos. Nulla congue pellentesque arcu, sit amet rutrum lorem bibendum eget. Nam quis quam gravida, placerat eros eu, ornare diam. Vestibulum consectetur orci sed lacus commodo pretium. Donec eu viverra lorem. Etiam facilisis
-          dapibus velit, non scelerisque eros aliquet ac. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-        <p>Morbi eros arcu, aliquet ac vehicula non, eleifend nec turpis. Cras in vulputate neque. Aliquam quis maximus tellus, vitae faucibus elit. Quisque consequat pellentesque lectus tempus pretium. Curabitur at metus et enim sollicitudin
-          convallis. Vestibulum tincidunt porta volutpat. Sed vel magna dui. Vestibulum elit quam, pharetra at mi sed, tincidunt facilisis nunc. Curabitur blandit nibh et risus pellentesque, nec placerat purus convallis. Quisque velit magna, mattis
-          eu magna ac, luctus tempus lorem. Nulla iaculis dolor malesuada elit consequat, ac sagittis ipsum mollis. Pellentesque a lacinia felis, sit amet facilisis felis. Cras pharetra nisl ac feugiat convallis. Suspendisse nec consectetur lectus.
-          Quisque metus est, venenatis in consectetur vel, auctor a ipsum.</p>
-        <a class="button" href="#">Read More</a>
-      </article>
-      <article class="post">
-        <h2>Blog Post 3</h2>
-        <p class="meta">Posted at 11:00 on May 9 by admin</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non tincidunt ante, eget pulvinar mauris. Integer commodo enim ac tincidunt molestie. In finibus, velit ac ultricies imperdiet, nunc lorem rhoncus eros, nec imperdiet dui
-          ipsum sit amet lectus. Nulla pulvinar, lectus in aliquet porttitor, lectus magna fermentum justo, viverra pretium sapien neque ut mauris. Donec sed ullamcorper ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-          inceptos himenaeos. Nulla congue pellentesque arcu, sit amet rutrum lorem bibendum eget. Nam quis quam gravida, placerat eros eu, ornare diam. Vestibulum consectetur orci sed lacus commodo pretium. Donec eu viverra lorem. Etiam facilisis
-          dapibus velit, non scelerisque eros aliquet ac. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-        <p>Morbi eros arcu, aliquet ac vehicula non, eleifend nec turpis. Cras in vulputate neque. Aliquam quis maximus tellus, vitae faucibus elit. Quisque consequat pellentesque lectus tempus pretium. Curabitur at metus et enim sollicitudin
-          convallis. Vestibulum tincidunt porta volutpat. Sed vel magna dui. Vestibulum elit quam, pharetra at mi sed, tincidunt facilisis nunc. Curabitur blandit nibh et risus pellentesque, nec placerat purus convallis. Quisque velit magna, mattis
-          eu magna ac, luctus tempus lorem. Nulla iaculis dolor malesuada elit consequat, ac sagittis ipsum mollis. Pellentesque a lacinia felis, sit amet facilisis felis. Cras pharetra nisl ac feugiat convallis. Suspendisse nec consectetur lectus.
-          Quisque metus est, venenatis in consectetur vel, auctor a ipsum.</p>
-        <a class="button" href="#">Read More</a>
-      </article>
+    <?php if (have_posts()) : ?>
+      <?php while (have_posts()) : the_post(); ?>
+        <article class="post">
+          <h2>Blog Post 1</h2>
+          <p class="meta">Posted at 11:00 on May 9 by admin</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non tincidunt ante, eget pulvinar mauris. Integer commodo enim ac tincidunt molestie. In finibus, velit ac ultricies imperdiet, nunc lorem rhoncus eros, nec imperdiet dui
+            ipsum sit amet lectus. </p>
+          <a class="button" href="#">Read More</a>
+        </article>
+      <?php endwhile; ?>
+    <?php else : ?>
+      <?php echo apautop('Sorry, no posts were found'); ?>
+    <?php endif; ?>
     </div>
     <div class="side">
       <div class="block">
